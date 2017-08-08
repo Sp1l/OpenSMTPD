c3a80bdcfd5d7f0df67a4ca8e8b5912e7ca8f24a
Gilles Chehade <gilles@poolp.org>
2017-08-08 22:25:05 +0200

    when loading queue at startup, skip and warn for envelopes with missing dispatcher

--- smtpd/queue.c
+++ smtpd/queue.c
@@ -715,6 +715,7 @@ static void
 queue_timeout(int fd, short event, void *p)
 {
 	static uint32_t	 msgid = 0;
+	struct dispatcher *dsp;
 	struct envelope	 evp;
 	struct event	*ev = p;
 	struct timeval	 tv;
@@ -733,6 +734,13 @@ queue_timeout(int fd, short event, void *p)
 	}
 
 	if (r) {
+		dsp = dict_get(env->sc_dispatchers, evp.dispatcher);
+		if (dsp == NULL) {
+			log_warnx("warn: queue: missing dispatcher \"%s\""
+			    " for envelope %016"PRIx64", ignoring",
+			    evp.dispatcher, evp.id);
+			goto reset;
+		}
 		if (msgid && evpid_to_msgid(evp.id) != msgid) {
 			m_create(p_scheduler, IMSG_QUEUE_MESSAGE_COMMIT,
 			    0, 0, -1);
@@ -745,6 +753,7 @@ queue_timeout(int fd, short event, void *p)
 		m_close(p_scheduler);
 	}
 
+reset:
 	tv.tv_sec = 0;
 	tv.tv_usec = 10;
 	evtimer_add(ev, &tv);
